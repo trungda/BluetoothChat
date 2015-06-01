661c8376023399728f649c872e3ddc8d490df95b
Trung Dinh <dinhanhtrung@gmail.com>
2015-06-01 12:06:26 +0900

    Connect each fragment to xml.

--- app/src/main/java/global/com/bluetoothchat/GroupFragment.java
+++ app/src/main/java/global/com/bluetoothchat/GroupFragment.java
@@ -1,9 +1,19 @@
 package global.com.bluetoothchat;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by dinhanhtrung on 5/25/15.
  */
 public class GroupFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_group, container, false);
+        return rootView;
+    }
 }
--- app/src/main/java/global/com/bluetoothchat/OneVsOneFragment.java
+++ app/src/main/java/global/com/bluetoothchat/OneVsOneFragment.java
@@ -1,9 +1,21 @@
 package global.com.bluetoothchat;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by dinhanhtrung on 5/25/15.
  */
 public class OneVsOneFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_onevsone, container, false);
+        return rootView;
+    }
+
 }
