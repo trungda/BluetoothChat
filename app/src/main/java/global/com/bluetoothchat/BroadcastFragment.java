f40ab0c8d737bcf49c691b7eb62702fbb2008fb9
zuo <z07diac@gmail.com>
2015-06-05 10:38:47 +0900

    add chat function

--- app/src/main/java/global/com/bluetoothchat/BroadcastFragment.java
+++ app/src/main/java/global/com/bluetoothchat/BroadcastFragment.java
@@ -1,6 +1,7 @@
 package global.com.bluetoothchat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -14,9 +15,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -24,19 +33,70 @@ import android.widget.TextView;
  */
 public class BroadcastFragment extends Fragment {
 
+
+    private  View rootView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_broadcast, container, false);
+        rootView = inflater.inflate(R.layout.fragment_broadcast, container, false);
+        ListView tv1 = (ListView) rootView.findViewById(R.id.listview);
+
+
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(),getData(),R.layout.list2,
+                new String[]{"title","info","img"},
+                new int[]{R.id.title,R.id.info,R.id.img});
+
+        tv1.setAdapter(adapter);
         return rootView;
     }
 
-    public void onCreateEditText(
-            Bundle savedInstanceState) {
-        EditText et = (EditText) getView().findViewById(R.id.et_sendmessage);
-        String str = et.getText().toString();
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
-        ListView tv1 = (ListView)getView().findViewById(R.id.listview);
-        //  tv1.setTextAlignment();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hello ");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hello");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hiiii");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hiiii");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+        map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hiiii");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+        map = new HashMap<String, Object>();
+        map.put("title", "Device1");
+        map.put("info", "hiiii");
+        map.put("img", R.drawable.i1);
+        list.add(map);
+
+        return list;
     }
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/global/com/bluetoothchat/OneVsOneFragment.java
+++ app/src/main/java/global/com/bluetoothchat/OneVsOneFragment.java
@@ -1,9 +1,46 @@
 package global.com.bluetoothchat;
 
-import android.support.v4.app.Fragment;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.support.v7.app.ActionBarActivity;
+        import android.support.v7.app.ActionBar;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentTransaction;
+        import android.support.v4.app.FragmentPagerAdapter;
+        import android.os.Bundle;
+        import android.support.v4.view.ViewPager;
+        import android.view.LayoutInflater;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ListView;
+        import android.widget.SimpleAdapter;
+        import android.widget.TextView;
+
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
+import android.widget.SimpleAdapter;
 
 /**
  * Created by dinhanhtrung on 5/25/15.
  */
 public class OneVsOneFragment extends Fragment {
+    private  View rootView;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_onevsone_chat, container, false);
+
+
+
+        return rootView;
+    }
 }
